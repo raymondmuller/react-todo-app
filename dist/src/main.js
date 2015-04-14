f7a540be29b24c3cca2caa8d32a82b85140067b3
rmuller17 <Xpl0sive>
2015-04-15 00:38:48 +0200

    clean up

--- dist/src/main.js
+++
@@ -1,3 +0,0 @@
-
-React.render(React.createElement("h1", null, "A Simple Todo App In ReactJS"), document.getElementById('header'));
-React.render(React.createElement(App, null), document.getElementById('app'));
