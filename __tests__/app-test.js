d2ac2981bdbb8f4e7178c6c6996c853e99880fdb
rmuller17 <Xpl0sive>
2015-04-15 22:45:44 +0200

    clean up

--- __tests__/app-test.js
+++ __tests__/app-test.js
@@ -1,27 +1,18 @@
-// __tests__/sum-test.js
 global.React = require('../bower_components/react/react-with-addons');
 global.TestUtils = React.addons.TestUtils;
 
-jest.dontMock('../src/js/todoInput');
+var Component, App;
+
+jest.dontMock('../src/js/app');
 
 describe('TodoInput', function() {
-
- it('test', function() {
-    var TodoInput = require('../src/js/todoInput');
- 	expect(TodoInput).not.toBeUndefined();
-
- 	var todoInput = TestUtils.renderIntoDocument(
- 			<TodoInput ref='todo' className="todo-input"></TodoInput>
- 		)
- 	console.log(todoInput)
- 	expect(todoInput).toEqual(true);
- 	var input = TestUtils.findRenderedDOMComponentWithClass(todoInput,'todo-input');
- 	expect(input).toEqual("test");	
-
 	
- 	expect(input).not.toBeUndefined();
+  beforeEach(function() {
+  	App = require('../src/js/app');
+    Component = TestUtils.renderIntoDocument(<App></App>);
+  });
 
-
-
+ it('should render', function() {
+ 	expect(App).toBeDefined();
  });
 });
