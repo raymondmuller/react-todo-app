e3811dc9bc3a1b53ed1095c7659ceb3cb9085d0c
rmuller17 <Xpl0sive>
2015-04-15 22:59:33 +0200

    structure change

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,9 @@ var path = {
   ALL: ['src/components/*.js', 'src/components/**/*.js', 'src/index.html', 'src/style/*.scss'],
   JS: ['src/components/*.js', 'src/components/**/*.js'],
   MINIFIED_OUT: 'build.min.js',
-  DEST_SRC: 'dist/src',
-  DEST_CSS: 'dist/src/css',
+  DEST_COMPONENTS: 'dist/components',
+  DEST_VENDOR: 'dist/vendor',
+  DEST_CSS: 'dist/css',
   DEST_BUILD: 'dist/build',
   DEST: 'dist'
 };
@@ -25,7 +26,7 @@ var path = {
 gulp.task('transform', function(){
   gulp.src(path.JS)
     .pipe(react())
-    .pipe(gulp.dest(path.DEST_SRC));
+    .pipe(gulp.dest(path.DEST_COMPONENTS));
 });
 
 gulp.task('copy', function(){
@@ -53,7 +54,7 @@ gulp.task('jest', function () {
 gulp.task('scripts', function() {
   gulp.src(['./bower_components/react/react.js'])
   .pipe(vendor('vendor.js'))
-  .pipe(gulp.dest(path.DEST_SRC));  
+  .pipe(gulp.dest(path.DEST_VENDOR));  
 });
 
 gulp.task('watch', function(){
